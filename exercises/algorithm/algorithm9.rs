86682f93f968f2318082da23d0f0d193ed152396
yaoxin <2154327696@qq.com>
2025-10-04 11:51:34 -0700

    update 110 new

--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -38,14 +38,18 @@ where
 
     pub fn add(&mut self, value: T) {
         //TODO
-        self.count += 1;
         self.items.push(value);
+        self.count += 1;
         let mut idx = self.count;
-        
-        while idx > 1 && (self.comparator)(&self.items[idx], &self.items[self.parent_idx(idx)]) {
-            self.items.swap(idx, self.parent_idx(idx));
-            idx = self.parent_idx(idx);
-        }
+        while idx > 1 {
+            let parent = self.parent_idx(idx);
+            if (self.comparator)(&self.items[idx], &self.items[parent]) {
+                self.items.swap(idx, parent);
+                idx = parent;
+            } else {
+                break;
+            }
+        }  
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -66,7 +70,15 @@ where
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
         //TODO
-		0
+		 let left = self.left_child_idx(idx);
+        let right = self.right_child_idx(idx);
+        if right <= self.count && (self.comparator)(&self.items[right], &self.items[left]) {
+            right
+        } else if left <= self.count {
+            left
+        } else {
+            0
+        }
     }
 }
 
@@ -87,13 +99,36 @@ where
 
 impl<T> Iterator for Heap<T>
 where
-    T: Default,
+    T: Default +Clone,
 {
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
         //TODO
-		None
+        if self.count == 0 {
+            return None;
+        }
+        let result = self.items[1].clone();
+        self.items[1] = self.items[self.count].clone();
+        self.items.pop();
+        self.count -= 1;
+
+        let mut idx = 1;
+        loop {
+            let child = self.smallest_child_idx(idx);
+            if child == 0 {
+                break;
+            }
+            if (self.comparator)(&self.items[child], &self.items[idx]) {
+                self.items.swap(idx, child);
+                idx = child;
+            } else {
+                break;
+            }
+        }
+        Some(result)
+        
+        
     }
 }
 
